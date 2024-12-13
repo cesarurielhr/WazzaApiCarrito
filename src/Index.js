4ce3cc8e7695ee870bff18c66d444f24862620a8
JoseLinoDC <joselinodiaz25@gmail.com>
2024-12-13 14:04:14 -0700

    Update Index.js

---
+++ src/Index.js
@@ -0,0 +1,27 @@
+// Importar módulos necesarios
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRoutes = require('./Routes/userRoutes');
+
+// Configurar el servidor Express
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Usar rutas
+app.use('/users', userRoutes);
+
+// Conexión a la base de datos y arranque del servidor
+mongoose.connect('mongodb+srv://admin:admin@cluster0.yelht.mongodb.net/tet01?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+}).catch(error => {
+    console.error('Error connecting to MongoDB:', error.message);
+});
